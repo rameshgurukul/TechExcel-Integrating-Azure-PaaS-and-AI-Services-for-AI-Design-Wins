c0800adf418b76500142aa840c5f647b621020f1
rameshgurukul <82775525+rameshgurukul@users.noreply.github.com>
2024-12-03 17:07:09 -0800

    Update 3_Vector_Search.py

--- src/ContosoSuitesDashboard/pages/3_Vector_Search.py
+++ src/ContosoSuitesDashboard/pages/3_Vector_Search.py
@@ -32,33 +32,31 @@ def main():
     )
 
     # Await a user message and handle the chat prompt when it comes in.
-    # col1 and col2 represent two separate columns in the Streamlit app.
-    # This allows us to format the application in a two-column layout.
     col1, col2 = st.columns(2)
 
-    # The "with col1" and "with col2" blocks define the content that will be displayed in each column.
     with col1:
         query = st.text_input("Search query:", key="query")
     with col2:
         max_results = st.text_input("Max results (<=0 will return all results):", key="max_results", value=0)
 
     minimum_similarity_score = st.slider("Minimum Similarity Score:", min_value=0.0, max_value=1.0, value=0.8, step=0.01)
-    
+
     if st.button("Submit"):
         with st.spinner("Performing vector search..."):
             if query:
                 # Vectorize the query text.
                 # Exercise 3 Task 3 TODO #4: Get the vectorized query text by calling handle_query_vectorization.
-                
+                query_vector = handle_query_vectorization(query)
                 # Perform the vector search.
                 # Exercise 3 Task 3 TODO #5: Get the vector search results by calling handle_vector_search.
-                
+                vector_search_results = handle_vector_search(query_vector, max_results, minimum_similarity_score)
                 # Display the results.
                 st.write("## Results")
                 # Exercise 3 Task 3 TODO #6: Display the results as a table.
-                
+                st.table(vector_search_results.json())
             else:
                 st.warning("Please enter a query.")
+
 
 if __name__ == "__main__":
     main()
