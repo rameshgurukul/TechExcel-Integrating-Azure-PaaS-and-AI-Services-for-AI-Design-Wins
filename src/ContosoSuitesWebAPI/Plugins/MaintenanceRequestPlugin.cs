d405325e5de181853be5b8298aafa46fd5cf4dd8
rameshgurukul <82775525+rameshgurukul@users.noreply.github.com>
2024-12-04 11:30:44 -0800

    Update MaintenanceRequestPlugin.cs

--- src/ContosoSuitesWebAPI/Plugins/MaintenanceRequestPlugin.cs
+++ src/ContosoSuitesWebAPI/Plugins/MaintenanceRequestPlugin.cs
@@ -1,13 +1,11 @@
-﻿using System.ComponentModel; 
+using System.ComponentModel; 
 using ContosoSuitesWebAPI.Entities;
 using Microsoft.Azure.Cosmos;
+using Microsoft.SemanticKernel;
 // Exercise 5 Task 2 TODO #5: Add a library references support Semantic Kernel.
 
 namespace ContosoSuitesWebAPI.Plugins
 {
-    /// <summary>
-    /// The maintenance request plugin for creating and saving maintenance requests.
-    /// </summary>
     public class MaintenanceRequestPlugin(CosmosClient cosmosClient)
     {
         private readonly CosmosClient _cosmosClient = cosmosClient;
@@ -16,11 +14,9 @@ namespace ContosoSuitesWebAPI.Plugins
         // The function should be named "create_maintenance_request" and it should have a description
         // the accurately describes the purpose of the function, such as "Creates a new maintenance request for a hotel."
 
-        // Exercise 5 Task 2 TODO #7: Add Kernel as the first parameter to the function.
-        /// <summary>
-        /// Creates a new maintenance request for a hotel.
-        /// </summary>
-        public async Task<MaintenanceRequest> CreateMaintenanceRequest(int HotelId, string Hotel, string Details, int? RoomNumber, string? location)
+        [KernelFunction("create_maintenance_request")]
+        [Description("Creates a new maintenance request for a hotel.")]
+        public async Task<MaintenanceRequest> CreateMaintenanceRequest(Kernel kernel, int HotelId, string Hotel, string Details, int? RoomNumber, string? location)
         {
             try
             {
@@ -48,11 +44,9 @@ namespace ContosoSuitesWebAPI.Plugins
         // The function should be named "save_maintenance_request" and it should have a description
         // the accurately describes the purpose of the function, such as "Saves a maintenance request to the database for a hotel."
 
-        // Exercise 5 Task 2 TODO #9: Add Kernel as the first parameter to the function.
-        /// <summary>
-        /// Saves a maintenance request to the database for a hotel.
-        /// </summary>
-        public async Task SaveMaintenanceRequest(MaintenanceRequest maintenanceRequest)
+        [KernelFunction("save_maintenance_request")]
+        [Description("Saves a maintenance request to the database for a hotel.")]
+        public async Task SaveMaintenanceRequest(Kernel kernel, MaintenanceRequest maintenanceRequest)
         {
             var db = _cosmosClient.GetDatabase("ContosoSuites");
             var container = db.GetContainer("MaintenanceRequests");
